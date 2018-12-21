a0938081c5b8e33cb6b54eda58c1359c7082be14
JEONG HYUN SEOK <nicewook@hotmail.com>
2018-12-22 08:27:31 +0900

    Impl ParseTime() function to pass timebot_test
    1. have to convert "PST" to "-0800"
    2. need better solution for all occasion

--- timebot/timebot.go
+++ timebot/timebot.go
@@ -3,10 +3,24 @@
 package timebot
 
 import (
+	"fmt"
 	"time"
+	"strings"
 )
 
 // ParseTime takes a string and returns time.Time in time.UTC
 func ParseTime(text string) (time.Time, bool) {
-	return time.Time{}, true
+	const longForm = "2006-01-02 15:04 -0700"
+
+	text = strings.Replace(text, "PST", "-0800", 1)
+	text = strings.Replace(text, "KST", "+0900", 1)
+	
+	isParsed := true
+	t, err := time.Parse(longForm, text)
+	if err != nil {
+		fmt.Println(err)
+		isParsed = false
+	}
+	t = t.UTC()
+	return t, isParsed
 }
